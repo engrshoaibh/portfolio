a8bee44afe9d38e674ba16f8717f9f604fdbed2b
Shoaib Hassan <165199701+engrshoaibh@users.noreply.github.com>
2024-10-03 17:11:43 +0500

    Updated the Netlify Configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        // Basic redirect
+        {
+          source: '/about',
+          destination: '/',
+          permanent: true,
+        },
+        // Wildcard path matching
+        {
+          source: '/blog/:slug',
+          destination: '/news/:slug',
+          permanent: true,
+        },
+        // Catch-all redirect for undefined routes
+        {
+          source: '/:path*',
+          destination: '/',
+          permanent: false, // Can be true if you want it as a permanent redirect (301)
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
