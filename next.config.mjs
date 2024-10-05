24f229d21d8ac9b830ff766d9317201c1799bbd3
Shoaib Hassan <165199701+engrshoaibh@users.noreply.github.com>
2024-10-05 16:15:33 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
+  
     async redirects() {
       return [
         {
-          source: '/about',
-          destination: '/',
-          permanent: true, 
+          source: '/:path*', // Matches all routes
+          destination: '/',  // Redirects to the homepage
+          permanent: true,   // Indicates a permanent redirect (status 308)
         },
-       
       ];
     },
-  };
+  }
   
   export default nextConfig;
   
